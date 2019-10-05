0d104050d97fb6cb4478fac26407dcf8ff9c9cbf
Theophile Kalumbu <kalutheo@hotmail.com>
2019-10-05 16:08:51 +0200

    feat(ui): add better css for form

--- src/Form.elm
+++ src/Form.elm
@@ -7,14 +7,50 @@ import Html.Events exposing (..)
 import Model exposing (..)
 
 
+viewFieldRow children =
+    div [ class "flex m-2" ]
+        children
+
+
+customInput inputType placeholderText toMsg =
+    let
+        inputClass =
+            "font-sans p-1 w-64 shadow appearance-none border rounded  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+    in
+    input [ onInput toMsg, type_ inputType, placeholder placeholderText, class inputClass ] []
+
+
+customButton =
+    let
+        buttonClass =
+            "font-sans bg-orange-400 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+    in
+    button [ class buttonClass ] [ text "Générer une annonce" ]
+
+
+customTitle titleText =
+    span [ class "text-xl ml-2 font-sans text-gray-600" ] [ text titleText ]
+
+
 view : Html Msg
 view =
-    Html.form []
-        [ input [ onInput (ChangeField StartDate), type_ "date", placeholder "start date" ] []
-        , input [ onInput (ChangeField EndDate), type_ "date", placeholder "end date" ] []
-        , input [ onInput (ChangeField MinAge), type_ "number", placeholder "min age" ] []
-        , input [ onInput (ChangeField MaxAge), type_ "number", placeholder "max age" ] []
-        , button [] [ text "Ok" ]
+    Html.form [ class "flex h-screen w-screen flex-col p-3", style "width" "600px" ]
+        [ div [ class "mb-4" ]
+            [ customTitle "Durée du séjour"
+            , viewFieldRow
+                [ div [ class "mr-2" ] [ customInput "date" "start date" (ChangeField StartDate) ]
+                , div [] [ customInput "date" "end date" (ChangeField EndDate) ]
+                ]
+            ]
+        , div
+            []
+            [ customTitle "Tranche d'age"
+            , viewFieldRow
+                [ div [ class "mr-2" ] [ customInput "number" "min age" (ChangeField MinAge) ]
+                , div [] [ customInput "number" "max age" (ChangeField MaxAge) ]
+                ]
+            ]
+        , viewFieldRow [ div [ class "float-right block" ] [ customButton ] ]
         ]
 
 
